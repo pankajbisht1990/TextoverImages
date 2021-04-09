a5d3b20803b4cfa02aceb506bd72b0e9c173d82e
pankaj bisht <bisht.pankaj90@gmail.com>
2021-04-09 10:17:45 +0530

    new changes

--- tester.php
+++ tester.php
@@ -1,9 +1,10 @@
 <?php
 /**
- * @author Amasty Team
+ * @author Amasty Team is for php developer
  * @copyright Copyright (c) 2015 Amasty (http://www.amasty.com)
  * @package Amasty_TextoverImages  tst
  */
+ echo "tester";
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Amasty_TextoverImages',
